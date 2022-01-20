475a7d669954c8eb57d9939e524253e3ab4b20d9
Agung Sepruloh <agungsepruloh1996@gmail.com>
2022-01-20 11:41:37 +0700

    Launch Odoo after open the app automatically

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import 'material_color.dart';
 
@@ -13,10 +14,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Elasri Lil Iskane',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: white),
       home: const MyHomePage(title: 'Elasri Lil Iskane'),
     );
   }
+}
+
+const String odooUrl = 'http://167.86.81.119:8069';
+
+Future<void> launchOdoo() async {
+  await launch(odooUrl);
 }
 
 class MyHomePage extends StatelessWidget {
@@ -29,12 +37,19 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    launchOdoo();
     return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.asset('assets/icons/icon.png'),
+            GestureDetector(
+              onTap: () => launchOdoo(),
+              child: Image.asset(
+                'assets/icons/splash.png',
+                height: MediaQuery.of(context).size.height / 3.5,
+              ),
+            ),
           ],
         ),
       ),
